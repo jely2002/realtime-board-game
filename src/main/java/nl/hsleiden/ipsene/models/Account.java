e8f2c44d35826615025b8624fd1a174b612217a9
Jelle Glebbeek <jw.glebbeek@gmail.com>
2021-04-26 16:53:49 +0200

    chore: add boilerplate project structure

---
+++ src/main/java/nl/hsleiden/ipsene/models/Account.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.ipsene.models;
+
+public class Account {
+
+  private final String uid = "";
+  private final String pwd = "";
+
+  public Account() {}
+
+  public boolean validateLogin(String uid, String pwd) {
+      return (uid.equals(this.uid)) && (pwd.equals(this.pwd));
+  }
+}
