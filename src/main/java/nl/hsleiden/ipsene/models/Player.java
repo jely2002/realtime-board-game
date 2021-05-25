298bfc3d54f664e6a17c4415604f9c851d4f1122
meesmb <60484127+meesmb@users.noreply.github.com>
2021-05-25 12:17:24 +0200

    chore: add basic model classes (#11)

---
+++ src/main/java/nl/hsleiden/ipsene/models/Player.java
@@ -0,0 +1,15 @@
+package nl.hsleiden.ipsene.models;
+
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Card> cards;
+
+    public Player() {
+        cards = new ArrayList<Card>();
+    }
+
+    public void addCard() {
+        // push back a card and check bounds
+    }
+}
