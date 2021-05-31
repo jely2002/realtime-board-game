e07a6f6a7a3b89c8b0c5e696443ce7595ebf14f5
Jelle Glebbeek <jw.glebbeek@gmail.com>
2021-05-31 13:49:09 +0200

    feat: add firebase and service

---
+++ src/main/java/nl/hsleiden/ipsene/firebase/Firebase.java
@@ -0,0 +1,29 @@
+package nl.hsleiden.ipsene.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Firebase {
+
+    private final Firestore store;
+
+    protected Firebase(String privateKeyPath) throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(privateKeyPath);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .build();
+        FirebaseApp.initializeApp(options);
+        this.store = FirestoreClient.getFirestore();
+    }
+
+    protected Firestore getStore() {
+        return store;
+    }
+}
