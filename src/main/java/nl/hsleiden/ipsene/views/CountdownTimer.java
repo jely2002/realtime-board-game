0f33ada42754b8cdae950e98fa8813994679098e
Tim Misiewicz <misiewicztim@gmail.com>
2021-06-09 21:44:23 +0200

    feat: added game timer view

--- src/main/java/nl/hsleiden/ipsene/views/CountdownTimer.java
+++ src/main/java/nl/hsleiden/ipsene/views/CountdownTimer.java
@@ -2,33 +2,58 @@ package nl.hsleiden.ipsene.views;
 
 import javafx.application.Platform;
 import javafx.scene.control.Label;
-import nl.hsleiden.ipsene.models.Board;
+import nl.hsleiden.ipsene.application.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class CountdownTimer implements Runnable{
-    static final double nanoFactor = Math.pow(10, 9);
-    static int maxTurnTime = 60;
-    private final Board board;
-    private Label label;
+    private static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
 
-    public CountdownTimer(Board board, Label label) {
-        this.board = board;
+    private int turnTime;
+    private final Label label;
+    private final int labelX;
+    private final int labelY;
+
+    private final int yOffset = 30;
+    private final int xOffset = 40;
+
+    public CountdownTimer(Label label, int turnTime, int labelX, int labelY ) {
         this.label = label;
+        this.turnTime = turnTime;
+        this.labelX = labelX;
+        this.labelY = labelY;
     }
 
     @Override
     public void run() {
-        while (maxTurnTime > 0){
-            Platform.runLater(() -> {
-                    label.setText(String.valueOf(maxTurnTime));
-                    });
-
-            maxTurnTime--;
+        ViewHelper.setNodeCoordinates(label, labelX, labelY);
+        label.setStyle("-fx-font-family: 'Comic Sans MS'; -fx-font-size: 120; -fx-text-fill: #000000");
+        while (turnTime > 10){
+            Platform.runLater(() -> label.setText(String.valueOf(turnTime)));
+            turnTime--;
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                logger.error(e.getMessage(), e);
+                break;
             }
         }
+        while (turnTime <= 10 && turnTime > 0 ){
+            Platform.runLater(() -> label.setText( "0" + turnTime));
+
+            turnTime--;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage(), e);
+                break;
+            }
+        }
+        Platform.runLater(() -> {
+            label.setStyle("-fx-font-family: 'Comic Sans MS'; -fx-font-size: 40; -fx-text-fill: #000000");
+            ViewHelper.setNodeCoordinates(label, labelX - xOffset, labelY + yOffset);
+            label.setText("Time's up!");
+        });
     }
 }
