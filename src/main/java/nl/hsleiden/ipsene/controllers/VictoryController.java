3df5e8b3b5bc70ee9c0cf5bd8025d4b804f0ecd0
Tim Misiewicz <misiewicztim@gmail.com>
2021-06-11 22:21:00 +0200

    feat: added VictoryView and controller

---
+++ src/main/java/nl/hsleiden/ipsene/controllers/VictoryController.java
@@ -0,0 +1,27 @@
+package nl.hsleiden.ipsene.controllers;
+
+import com.google.cloud.firestore.DocumentSnapshot;
+import javafx.stage.Stage;
+import nl.hsleiden.ipsene.firebase.FirebaseService;
+import nl.hsleiden.ipsene.interfaces.Controller;
+import nl.hsleiden.ipsene.interfaces.View;
+
+import java.io.IOException;
+
+public class VictoryController implements Controller {
+    @Override
+    public void update(DocumentSnapshot ds) {
+
+    }
+
+    @Override
+    public void registerObserver(View v) {
+
+    }
+
+    public void backToMainMenu(Stage primaryStage) throws IOException {
+        FirebaseService firebaseService =
+            new FirebaseService("/firestoretest-5c4e4-52601abc4d0c.json", "games");
+      LobbyController lobbyController = new LobbyController(firebaseService, primaryStage);
+    }
+}
