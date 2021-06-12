e6aceabab460c61c919f1645b66d18e59fb325cc
meesmb <mrmees24@gmail.com>
2021-06-12 12:17:00 +0200

    removed

--- src/main/java/nl/hsleiden/ipsene/views/MenuView.java
+++ src/main/java/nl/hsleiden/ipsene/views/MenuView.java
@@ -56,14 +56,7 @@ public class MenuView implements View {
     }
     lobbyController.registerObserver(this);
 
-    // todo remove this when done debugging
-        try {
-          // lobbyController.join("76430");
-          lobbyController.host();
-          toLobby();
-        } catch (ServerConnectionException e) {
-          e.printStackTrace();
-        }
+
   }
 
   private void loadPrimaryStage(Pane pane) {
