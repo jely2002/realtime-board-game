8454a476861e0a2e736022cbe8febbca13d95778
Frank van den Broek <frankvandenbroek@protonmail.com>
2021-06-12 15:09:16 +0200

    fix: make opening in browser dependent on host OS

--- src/main/java/nl/hsleiden/ipsene/controllers/BoardController.java
+++ src/main/java/nl/hsleiden/ipsene/controllers/BoardController.java
@@ -1,14 +1,17 @@
 package nl.hsleiden.ipsene.controllers;
 
 import com.google.cloud.firestore.DocumentSnapshot;
+import com.sun.javafx.PlatformUtil;
 import nl.hsleiden.ipsene.interfaces.Controller;
 import nl.hsleiden.ipsene.interfaces.View;
 import nl.hsleiden.ipsene.models.Board;
 import nl.hsleiden.ipsene.models.Deck;
-
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BoardController implements Controller {
+
+  private static final Logger logger = LoggerFactory.getLogger(BoardController.class.getName());
 
   static BoardController boardController = null;
   private Board board;
@@ -56,9 +59,17 @@ public class BoardController implements Controller {
 
   public static void openWebpage(String url) {
     try {
-      java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
-    } catch (java.io.IOException e) {
-      System.out.println(e.getMessage());
+      if (PlatformUtil.isWindows()) {
+        java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+      }
+      if (PlatformUtil.isLinux()) {
+        Runtime.getRuntime().exec(String.format("xdg-open " + url));
+      }
+      if (PlatformUtil.isMac()) {
+        Runtime.getRuntime().exec(String.format("open " + url));
+      }
+    } catch (Exception e) {
+      logger.warn(e.getMessage(), e);
     }
   }
 }
